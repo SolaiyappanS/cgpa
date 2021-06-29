dadd4ba2e2f495ec6f6c9c67d8e40b69015c29d5
Solai <solaiyappatce@gmail.com>
2021-06-29 09:04:58 +0530

    update 7 and 8

--- backend.js
+++ backend.js
@@ -5,6 +5,8 @@ function calculate() {
     var gpa4 = document.getElementById("gpa4").value;
     var gpa5 = document.getElementById("gpa5").value;
     var gpa6 = document.getElementById("gpa6").value;
+    var gpa7 = document.getElementById("gpa7").value;
+    var gpa8 = document.getElementById("gpa8").value;
     var cred1 = document.getElementById("cred1").value;
     cred1 = Number(cred1);
     var cred2 = document.getElementById("cred2").value;
@@ -17,9 +19,12 @@ function calculate() {
     cred5 = Number(cred5);
     var cred6 = document.getElementById("cred6").value;
     cred6 = Number(cred6);
+    var cred7 = document.getElementById("cred7").value;
+    cred7 = Number(cred7);
+    var cred8 = document.getElementById("cred8").value;
+    cred8 = Number(cred8);
 
-    var result = (gpa1*cred1 + gpa2*cred2 + gpa3*cred3 + gpa4*cred4 + gpa5*cred5 + gpa6*cred6)/(cred1+cred2+cred3+cred4+cred5+cred6);
-    //var result =(cred1+cred2+cred3+cred4+cred5+cred6);
+    var result = (gpa1*cred1 + gpa2*cred2 + gpa3*cred3 + gpa4*cred4 + gpa5*cred5 + gpa6*cred6 + gpa7*cred7 + gpa8*cred8)/(cred1+cred2+cred3+cred4+cred5+cred6+cred7+cred8);
 
     document.getElementById("result").innerHTML= "Your CGPA is "+result+"<br>Round off to 2 or 3 decimal places";
 
